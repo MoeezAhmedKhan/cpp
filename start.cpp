0318821773b4373a6d09cfdf060a231aea73dc42
Moeez Ahmed Khan <145145714+MoeezAhmedKhan@users.noreply.github.com>
2026-04-01 01:02:48 +0500

    start c++

---
+++ start.cpp
@@ -0,0 +1,17 @@
+// Step 1: Install MinGW and add its 'bin' folder to environment variables
+
+// Step 2: Install required VS Code extensions:
+// - C/C++
+// - Code Runner
+
+// Step 3: Compile the program using:
+// g++ start.cpp -o start
+// (-o is used to rename the output executable file)
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello";
+    return 0;
+}
