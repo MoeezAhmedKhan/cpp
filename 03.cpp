4557458c5c6affca906f1b51de57fc9653d9c3f6
Moeez Ahmed Khan <145145714+MoeezAhmedKhan@users.noreply.github.com>
2026-04-07 06:24:06 +0500

    03.file created

---
+++ 03.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Patterns
+    // We are using for loop because we will use majorly for loop in future also. We will loop only importants question in pattern
+    // Will cover nested loops through patterns
+
+
+    // 1. Square Pattern:
+    // Example:
+    // 1 2 3 4 
+    // 1 2 3 4
+    // 1 2 3 4
+    // 1 2 3 4
+    
+    // It shape will depended on n if n is 4 so it show above shape, And If n is 3 so it will not include 4 in row and column also
+    // Hint: Outer loop will be run on 'n' times
+
+    
+
+
+
+    return 0;
+}
